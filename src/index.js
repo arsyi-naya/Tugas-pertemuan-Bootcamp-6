c85bd71b7ec4b7550a3898e638d44249e4f9c452
Arsyifa <nazhayaarsyifa@gmail.com>
2026-06-18 18:44:41 +0700

    index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
   res.send("API jalan");
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server jalan di port " + PORT);
+  console.log("server jalan");
 });
